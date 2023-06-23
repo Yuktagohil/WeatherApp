a39aeae90cc6345d5d31891e0b7b273a19290295
yuktaba.gohil <yuktaba.gohil@letsnurture.co.in>
2023-06-23 14:38:51 -0400

    final

--- src/services/weatherService.js
+++ src/services/weatherService.js
@@ -1,6 +1,6 @@
 import { DateTime } from "luxon";
 
-const API_KEY = "8ca9fca2fd30cd8b047134fb00db4ee3";
+const API_KEY = "Put your API Key";
 const BASE_URL = "https://api.openweathermap.org/data";
 
 // https://api.openweathermap.org/data/3.0/onecall?lat=48.8534&lon=2.3488&exclude=current,minutely,hourly,alerts&appid=1fa9ff4126d95b8db54f3897a208e91c&units=metric
